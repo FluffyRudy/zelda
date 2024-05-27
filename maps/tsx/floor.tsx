27369eaa707437be11d631722b11695b215a3732
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-05-27 20:44:06 +0545

    add tilesets and map

---
+++ maps/tsx/floor.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Floor" tilewidth="64" tileheight="64" tilecount="550" columns="22">
+ <image source="../../graphics/tilemap/Floor.png" width="1408" height="1600"/>
+ <wangsets>
+  <wangset name="cornor" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="70" wangid="0,0,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
