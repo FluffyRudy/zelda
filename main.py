16a3f9e3202be5090904668363572da64af7a25c
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-05-27 20:44:13 +0545

    add map

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from typing import Optional
 import sys
 from settings import WIDTH, HEIGHT,TILESIZE, FPS
 import pygame
-
-
+from level import Level
 
 class Game:
     BLACK = pygame.Color(0, 0, 0, 255)
 
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption('Might And Magic')
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
     
     def handle_event(self) -> Optional[None]:
         for event in pygame.event.get():
@@ -23,6 +25,7 @@ class Game:
         while True:
             self.handle_event()
             self.screen.fill(self.BLACK)
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
