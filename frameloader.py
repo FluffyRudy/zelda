64b19876e525b936922c7115a5fda04f242de64b
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-05-28 14:41:40 +0545

    animation frameloader utility

---
+++ frameloader.py
@@ -0,0 +1,17 @@
+import pygame
+import os
+
+def load_frames(path: str):
+    if not os.path.exists(path):
+        return []
+
+    valid_extension = '.png'
+    images = []
+
+    for file in os.listdir(path):
+        if os.path.splitext(file)[1] == valid_extension:
+            full_relative_path = os.path.join(path, file)
+            image = pygame.image.load(full_relative_path).convert_alpha()
+            images.append(image)
+    
+    return images
