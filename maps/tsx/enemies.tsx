76cf5cfd91be19abd4a9523168bd80cfcd5f903e
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-01 20:54:59 +0545

    edit map

---
+++ maps/tsx/enemies.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="enemies" tilewidth="240" tileheight="240" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="monster_type" value="bamboo"/>
+  </properties>
+  <image width="64" height="64" source="../../graphics/monsters/bamboo/attack/0.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="monster_type" value="raccoon"/>
+  </properties>
+  <image width="240" height="240" source="../../graphics/monsters/raccoon/idle/0.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="monster_type" value="spirit"/>
+  </properties>
+  <image width="64" height="64" source="../../graphics/monsters/spirit/idle/0.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="monster_type" value="squid"/>
+  </properties>
+  <image width="64" height="64" source="../../graphics/monsters/squid/attack/0.png"/>
+ </tile>
+</tileset>
