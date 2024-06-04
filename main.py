4954128f51dcbbacf664b6f905fe4d91a6f535f0
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-05 01:19:16 +0545

    temporarily add viewpost surface to change the contrast

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class Game:
         pygame.init()
         pygame.display.set_caption("ZELDA")
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.viewport = pygame.Surface(self.screen.get_size(), pygame.SRCALPHA)
+        self.viewport_bg = pygame.Color(0, 0, 0, 0)
         self.clock = pygame.time.Clock()
 
         self.level = Level()
@@ -21,12 +23,15 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.viewport_bg.a = min(self.viewport_bg.a + 10, 255)
 
     def run(self) -> None:
         while True:
             self.handle_event()
-            self.screen.fill(self.BLACK)
             self.level.run()
+            self.viewport.fill(self.viewport_bg)
+            self.screen.blit(self.viewport, (0, 0))
             pygame.display.update()
             self.clock.tick(FPS)
 
