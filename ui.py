8be2ad478b4ffb840212e66d087d5619107fa724
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-05 00:56:43 +0545

    add exp

--- ui.py
+++ ui.py
@@ -21,8 +21,9 @@ class UI:
 
         self.get_current_magic = player.magic_handler.get_current_magic
 
-        self.set_current_health = player.get_current_health
-        self.set_current_energy = player.get_current_energy
+        self.get_player_health = player.get_current_health
+        self.get_player_energy = player.get_current_energy
+        self.get_player_exp = player.get_current_exp
 
         self.weapon_image = player.weapon_handler.weapon.copy()
         self.weapon_pos = (
@@ -46,6 +47,10 @@ class UI:
 
         self.lower_health_layer_rect = self.health_bar_rect.copy()
         self.lower_energy_layer_rect = self.energy_bar_rect.copy()
+        self.exp_label_pos = (
+            self.offset_x,
+            self.lower_energy_layer_rect.bottom + self.offset_y,
+        )
 
         self.item_box_surf = pygame.Surface(
             (ITEM_BOX_SIZE * 2 + self.offset_x, ITEM_BOX_SIZE), pygame.SRCALPHA
@@ -67,10 +72,12 @@ class UI:
         pygame.draw.rect(
             self.display_surface, LOWER_LAYER_COLOR, self.lower_energy_layer_rect, 0, 3
         )
+        exp_label = self.font.render(f"{self.get_player_exp()}", True, "white", "black")
+        self.display_surface.blit(exp_label, self.exp_label_pos)
         pygame.draw.rect(self.display_surface, HEALTH_COLOR, self.health_bar_rect, 0, 3)
         pygame.draw.rect(self.display_surface, ENERGY_COLOR, self.energy_bar_rect, 0, 3)
-        self.health_bar_rect.width = self.set_current_health()
-        self.energy_bar_rect.width = self.set_current_energy()
+        self.health_bar_rect.width = self.get_player_health()
+        self.energy_bar_rect.width = self.get_player_energy()
         self.display_surface.blit(self.item_box_surf, self.item_box_pos)
         self.weapon_overlay()
         self.magic_overlay()
